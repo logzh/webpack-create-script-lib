472148110e31f2084f38bfd87f90e20e2fb85266
logzh <zjhcug@qq.com>
2016-11-21 12:51:17 +0800

    create a vue component lib

--- app/components/hello.vue
+++ app/components/hello.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="hello">
-        <div>this is template body</div>
+        <div>A custom component</div>
         <div>{{message}}</div>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                {{index}}:{{ todo.text }}
+            </li>
+        </ul>
     </div>
 </template>
 <style scoped>
@@ -13,6 +18,7 @@
     //    import HeaderComponent from './components/header.vue'
     //    import OtherComponent from './components/other.vue'
     export default{
+        props:['todos'],
         data(){
             return{
                 message:'hello vue'
