08c389371222446ddc5adde8cc5c3c49a8319444
logzh <zjhcug@qq.com>
2017-02-07 00:03:08 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // var $ = require("jquery");
-var Foo = require("./js/Foo");
+// var Foo = require("./js/Foo");
 var HelloVue = require("./components/HelloVue.vue");
 // import HelloVue from './components/HelloVue.vue'
 // import HelloReact from './components/Hello/index.jsx'
 
 module.exports = {
-  fun1: Foo,
+  // fun1: Foo,
   HelloVue: HelloVue,
   // HelloReact: HelloReact
 };
