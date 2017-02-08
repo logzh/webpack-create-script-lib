4a7ab230a1614149e426a0caf1632b9e285eaecd
logzh <zjhcug@qq.com>
2017-02-08 21:42:26 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 var HelloVue = require("./components/HelloVue.vue");
 // import HelloVue from './components/HelloVue.vue'
 // import HelloReact from './components/Hello/index.jsx'
-
+var test = require('test');
 module.exports = {
   // fun1: Foo,
+  test: test,
   HelloVue: HelloVue,
   // HelloReact: HelloReact
 };
